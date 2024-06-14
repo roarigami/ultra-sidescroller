95524271f9400fdb60268863e30b29a33c576c78
Rory Kane <roarigami@gmail.com>
2024-06-13 20:40:05 -0400

    Overworld class

---
+++ js/overworld.js
@@ -0,0 +1,31 @@
+class Overworld {
+    constructor(config) {
+      this.element = config.element;
+      this.canvas = canvas10;
+      this.ctx = this.canvas.getContext('2d', {willReadFrequently: true});
+      this.map = null;
+      this.x = 0;
+    }
+
+    startGameLoop() {
+        const animate = () => {
+          this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+          this.ctx.fillRect(this.x, 50, 100, 100);
+          //this.x++;
+          // requestAnimationFrame(animate);
+          requestAnimationFrame(() => {
+              animate();
+          })
+        }
+        animate();
+    }
+
+    startMap() {
+
+    }
+
+    init() {
+      this.startGameLoop();
+    }
+
+}
