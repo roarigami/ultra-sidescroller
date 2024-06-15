d2b76d3b30518794add12c4cee551baabc7bcd6e
Rory Kane <roarigami@gmail.com>
2024-06-15 15:37:57 -0400

    Player Image
    Referencing player image with id and drawing single image to canvas

--- js/player.js
+++ js/player.js
@@ -9,6 +9,8 @@ class Player {
       this.height = 91.3;
       this.x = 0;
       this.y = this.game.height - this.height;
+
+      this.image = player;
   }
 
   update() {
@@ -17,7 +19,9 @@ class Player {
 
   draw(context) {
     context.clearRect(0, 0, this.game.canvas.width, this.game.canvas.height)
-    context.fillRect(this.x, this.y, this.width, this.height);
+    //context.fillRect(this.x, this.y, this.width, this.height);
+    context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y,
+    this.width, this.height);
     //this.x++;
   }
 
