dbcb9cd043e33803fe232842aa92765fff7a4ea5
Rory Kane <roarigami@gmail.com>
2024-06-15 17:47:05 -0400

    Horizontal motion
    Horizontal boundaries set. Character can't navigate off screen

--- js/player.js
+++ js/player.js
@@ -23,6 +23,8 @@ class Player {
     if(input.includes('ArrowRight')) this.speed = this.maxSpeed;
     else if(input.includes('ArrowLeft')) this.speed = -this.maxSpeed;
     else this.speed = 0;
+    if(this.x < 0) this.x = 0;
+    if(this.x > this.game.width - this.width) this.x = this.game.width - this.width;
   }
 
   draw(context) {
