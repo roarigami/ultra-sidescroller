97246e062ea470866376e6861862eb03e3795534
Rory Kane <roarigami@gmail.com>
2024-06-15 15:58:33 -0400

    Input handler
    key up event listener for player game controls

--- js/input.js
+++ js/input.js
@@ -11,8 +11,20 @@ class InputHandler {
                 this.keys.indexOf(e.key) === -1) {
               this.keys.push(e.key);
           } else if(e.key === 'd') this.game.debug = !this.game.debug;
-          console.log(e.key);
+          //console.log(e.key);
 
+      });
+      window.addEventListener('keyup', (e) => {
+          //console.log(e.key);
+          if(  e.key === 'ArrowDown' ||
+               e.key === 'ArrowUp' ||
+               e.key == 'ArrowLeft' ||
+               e.key == 'ArrowRight' ||
+               //e.key == 'Enter' ||
+               e.key == 'r') {
+              this.keys.splice(this.keys.indexOf(e.key), 1);
+          }
+          //console.log(this.keys);
       });
     }
 }
