67b8c98ef42fe34824a1ede00cb29846485e25e2
Rory Kane <roarigami@gmail.com>
2024-06-17 15:59:50 -0400

    Particle class
     particles class will be the parent (super) class motion effects during gameplay. Like dust when the player runs, fire trail in rollout and a fire splash when the player hits the ground on dive.

---
+++ js/particles.js
@@ -0,0 +1,12 @@
+class Particle {
+  constructor(game) {
+      this.game = game;
+      this.markedForDeletion = false;
+  }
+  update() {
+      this.x -= this.speedX + this.game.speed;
+      this.y -= this.speedY;
+      this.size *= 0.95;
+      if(this.size < 0.5) this.markedForDeletion = true;
+  }
+}
