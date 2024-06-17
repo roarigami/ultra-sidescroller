40cc988a659fb7e05cd79a71c40d2f5b79142ca1
Rory Kane <roarigami@gmail.com>
2024-06-17 08:09:36 -0400

    Enemy class
    Enemy parent class used to create the outline for different game enemies

---
+++ js/enemies.js
@@ -0,0 +1,17 @@
+class Enemy {
+    constructor() {
+      this.frameX = 0;
+      this.frameY = 0;
+      this.fps = 20;
+      this.frameInterval = 1000/this.fps;
+      this.frameTimer = 0;
+    }
+
+    update() {
+
+    }
+
+    draw() {
+      
+    }
+}
